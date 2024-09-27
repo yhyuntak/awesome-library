3bde46cc584d660e28a97f62267e55d3182523bb
yhyuntak <yhyuntak@gmail.com>
2024-09-27 14:41:36 +0900

    init

---
+++ src/main/java/com/example/exampleframework/ExampleframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleframeworkApplication.class, args);
+    }
+
+}
